093a75265b8fa1765006f688af9a9cfb93881bb9
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-17 21:01:55 +0300

    Add and use cookie parser middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 // Create Express server
 import express from "express";
 import authRouter from "./routers/auth.js";
+import cookieParser from "cookie-parser";
 
 // Create Express app
 const app = express();
+
+// use cookie parser middleware to parse cookies
+app.use(cookieParser());
 
 // use the auth router
 app.use("/auth", authRouter);
