dcdc7e8ab840f4d6755c7e5384f92d409cff6f30
Sherif <sherif3hassan@gmail.com>
2023-06-17 21:06:11 +0300

    Create register endpoint

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -8,14 +8,41 @@ router.get("/me", (req, res) => {
   res.send("Me");
 });
 
-router.post("/login", (req, res) => {
-  res.send("Login");
-});
+router.post(
+  "/register",
+  // Validate and sanitize the username and password fields
+  body("username").trim().notEmpty().isLength({ min: 3 }).escape().withMessage("Username must be at least 3 characters long"),
+  body("password").trim().notEmpty().isLength({ min: 8 }).escape().withMessage("Password must be at least 8 characters long"),
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-router.post("/register", (req, res) => {
-  res.send("Register");
-});
+    try {
+      // Check if the username already exists in the database
+      const exists = await User.exists({ username: req.body.username });
+      if (exists) {
+        return res.status(400).send("Username already exists");
+      }
 
+      // Create the user in the database
+      const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+});
+      // save the user in the database
+      await user.save();
+
+      // Return the newly created user or a success message
+      res.json({ user, message: "Registration successful" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 router.post("/logout", (req, res) => {
   res.send("Logout");
 });
