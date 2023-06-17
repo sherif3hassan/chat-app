e44ec7db48b2735cc50308fcb83c4f894efac4d2
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-17 20:01:00 +0300

    Add validations on Room schema
    For some reason, mongoDB doesn't have refrential integrity at database
    level, so we had to implement it ourselves using the validation field in
    the `mongoose.Schema` of the `Room`.

--- backend/models/room.js
+++ backend/models/room.js
@@ -1,4 +1,5 @@
 // import mongoose
+import User from "./user.js";
 import mongoose from "mongoose";
 
 // User model and schema
@@ -10,6 +11,21 @@ const messageSchema = new mongoose.Schema({
 const roomSchema = new mongoose.Schema({
   usersId: [mongoose.Schema.Types.ObjectId],
   messagesId: [messageSchema],
+        validate: {
+          validator: async (value) => {
+            const exists = await User.findById(value);
+            return exists !== null;
+          },
+        },
+        senderId: {
+          type: mongoose.Schema.Types.ObjectId,
+          validate: {
+            validator: async (value) => {
+              const exists = await User.findById(value);
+              return exists !== null;
+            },
+          },
+        },
 });
 
 // create the model for users and expose it to our app
