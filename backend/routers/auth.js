ffbfe23a16ce586c6e31be26c7466950da529781
Sherif <sherif3hassan@gmail.com>
2023-06-17 21:06:31 +0300

    Create login endpoint

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,5 +1,8 @@
 // Create the auth router
 import { Router } from "express";
+import { User } from "../models";
+import bcrypt from "bcrypt";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -7,6 +10,41 @@ const router = Router();
 router.get("/me", (req, res) => {
   res.send("Me");
 });
+
+router.post(
+  "/login",
+  body("username").trim().notEmpty().isLength({ min: 3 }).escape().withMessage("Username must be at least 3 characters long"),
+  body("password").trim().notEmpty().isLength({ min: 8 }).escape().withMessage("Password must be at least 8 characters long"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { username, password } = req.body;
+
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid username or password" });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(400).json({ message: "Invalid username or password" });
+      }
+
+      // Perform additional authentication logic if necessary
+
+      res.json({ user, message: "Login successful" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+    
+  
 
 router.post(
   "/register",
@@ -31,7 +69,7 @@ router.post(
       const user = await User.create({
         username: req.body.username,
         password: req.body.password,
-});
+      });
       // save the user in the database
       await user.save();
 
