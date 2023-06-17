eab2b61b4834d0e4d6753b7bdde06f1aaadd657d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-17 21:02:51 +0300

    Add and use express-session middleware

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 // Create Express server
 import express from "express";
 import authRouter from "./routers/auth.js";
+import sessions from "express-session";
 import cookieParser from "cookie-parser";
 
 // Create Express app
 const app = express();
+
+// use the sessions middleware
+app.use(
+  sessions({
+    secret: "potatoman",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 1 week
+    resave: false,
+  })
+);
 
 // use cookie parser middleware to parse cookies
 app.use(cookieParser());
